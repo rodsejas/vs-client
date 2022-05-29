fd84a252d4fcf3c92d7a06ae74106b477d7c2a1e
Rodrigo Sejas <rodsejas@gmail.com>
2022-05-29 13:48:14 +1000

    Auth state turned off for build until bug fixed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import {
   HashRouter as Router,
   Route,
   Routes,
-  Navigate,
+  // Navigate,
 } from "react-router-dom";
-import { useState, useContext, createContext } from "react";
+// import { useState, useContext, createContext } from "react";
 import Home from "./pages/Home";
 import Equipments from "./pages/Equipments";
 import Equipment from "./pages/Equipment";
@@ -24,140 +24,142 @@ import SignIn from "./pages/SignIn";
 
 // import NavBar from "./components/NavBar";
 
-const LoggedInContext = createContext(false);
+// const LoggedInContext = createContext(false);
 
-const RequireLoggedIn = ({ children }) => {
-  const isLoggedIn = useContext(LoggedInContext);
-  console.log("Is Logged In", isLoggedIn);
-  if (isLoggedIn) {
-    return children;
-  }
-  return <Navigate to="/signin" />;
-};
+// const RequireLoggedIn = ({ children }) => {
+//   const isLoggedIn = useContext(LoggedInContext);
+//   console.log("Is Logged In", isLoggedIn);
+//   if (isLoggedIn) {
+//     return children;
+//   }
+//   return <Navigate to="/signin" />;
+// };
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <LoggedInContext.Provider value={isLoggedIn}>
-      <div className="App">
-        <Router>
-          {/* <NavBar setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} /> */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route
-              path="/equipments"
-              element={
-                // <RequireLoggedIn>
-                <Equipments />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/equipment/:id"
-              element={
-                // <RequireLoggedIn>
-                <Equipment />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/equipment/:id/edit"
-              element={
-                // <RequireLoggedIn>
-                <EditEquipment />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/equipment/create"
-              element={
-                // <RequireLoggedIn>
-                <NewEquipment />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/workers"
-              element={
-                // <RequireLoggedIn>
-                <Workers />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/worker/:id"
-              element={
-                // <RequireLoggedIn>
-                <Worker />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/inspections"
-              element={
-                // <RequireLoggedIn>
-                <Inspections />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/inspection/:id"
-              element={
-                // <RequireLoggedIn>
-                <Inspection />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/equipment/:id/inspection/create"
-              element={
-                // <RequireLoggedIn>
-                <NewInspection />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/models"
-              element={
-                // <RequireLoggedIn>
-                <Models />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/model/:id"
-              element={
-                // <RequireLoggedIn>
-                <Model />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/model/:id/edit"
-              element={
-                // <RequireLoggedIn>
-                <EditModel />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route
-              path="/model/create"
-              element={
-                // <RequireLoggedIn>
-                <NewModel />
-                // </RequireLoggedIn>
-              }
-            />
-            <Route path="/signup" element={<SignUp />} />
-            <Route
-              path="/signin"
-              element={<SignIn setIsLoggedIn={setIsLoggedIn} />}
-            />
-          </Routes>
-        </Router>
-      </div>
-    </LoggedInContext.Provider>
+    // <LoggedInContext.Provider value={isLoggedIn}>
+    <div className="App">
+      <Router>
+        {/* <NavBar setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} /> */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/equipments"
+            element={
+              // <RequireLoggedIn>
+              <Equipments />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/equipment/:id"
+            element={
+              // <RequireLoggedIn>
+              <Equipment />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/equipment/:id/edit"
+            element={
+              // <RequireLoggedIn>
+              <EditEquipment />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/equipment/create"
+            element={
+              // <RequireLoggedIn>
+              <NewEquipment />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/workers"
+            element={
+              // <RequireLoggedIn>
+              <Workers />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/worker/:id"
+            element={
+              // <RequireLoggedIn>
+              <Worker />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/inspections"
+            element={
+              // <RequireLoggedIn>
+              <Inspections />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/inspection/:id"
+            element={
+              // <RequireLoggedIn>
+              <Inspection />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/equipment/:id/inspection/create"
+            element={
+              // <RequireLoggedIn>
+              <NewInspection />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/models"
+            element={
+              // <RequireLoggedIn>
+              <Models />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/model/:id"
+            element={
+              // <RequireLoggedIn>
+              <Model />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/model/:id/edit"
+            element={
+              // <RequireLoggedIn>
+              <EditModel />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route
+            path="/model/create"
+            element={
+              // <RequireLoggedIn>
+              <NewModel />
+              // </RequireLoggedIn>
+            }
+          />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/signin"
+            element={<SignIn />}
+
+            // element={<SignIn setIsLoggedIn={setIsLoggedIn} />}
+          />
+        </Routes>
+      </Router>
+    </div>
+    // </LoggedInContext.Provider>
   );
 }
 
